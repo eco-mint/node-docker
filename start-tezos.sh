6f0fcb59ed2f6b1bf3c8218562d41fd758b96844
glcohen <gabe.l.cohen@gmail.com>
2021-12-23 23:23:26 -0800

    allowlist

--- start-tezos.sh
+++ start-tezos.sh
@@ -5,7 +5,7 @@ init_node() {
 	tezos-node identity generate 26
 	tezos-node config init "$@" \
 		--rpc-addr="[::]:$rpcport" \
-		--allow-all-rpc="[::]:$rpcport" \
+		--allow-all-rpc="0.0.0.0:$rpcport" \
 		--net-addr="[::]:$netport" \
 		--connections=$connections \
 		--network=$network \
